56748884893780ea73bf68320a7c53e199f1a9a2
Marcolino5 <marcos.alex2004@gmail.com>
2023-04-23 18:37:53 -0300

    exercicios 1, 2 e 3

--- exercicios.v
+++ exercicios.v
@@ -1,5 +1,21 @@
 Section aula2.
-  Variables p q r: Prop.
+  Variables p q r t: Prop.
+
+    Lemma exercicio1: (p -> p -> q) -> p -> q.
+    Proof.
+      intro H1. intro H2. apply H1. apply H2. apply H2.
+    Qed.
+
+    Lemma exercicio2: (p -> q) -> (p -> p -> q).
+    Proof.
+      intro H1. intro H2. apply H1.
+    Qed.
+
+    Lemma exercicio3: (q -> r -> t) -> (p -> q) -> p -> r -> t.
+    Proof.
+      intro H1. intro H2. intro H3. apply H1. apply H2. apply H3.
+    Qed.
+
     Lemma exercicio4: (p -> q -> r) -> (p -> q) -> p -> r.
     Proof.
       intro H1. intro H2. intro H3.
