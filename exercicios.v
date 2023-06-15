658479d7d2940fd902a6b750f1e498354754e756
Marcolino5 <marcos.alex2004@gmail.com>
2023-06-14 23:29:20 -0300

    Exercicios 87 e 88

--- exercicios.v
+++ exercicios.v
@@ -1,3 +1,38 @@
+Section ex88e87.
+
+  (* Exercício 87 *)
+  Lemma PIG: forall (P : nat -> Prop) (k : nat), P k ->
+        (forall n, n >= k -> P n -> P (S n)) ->
+          forall n : nat, n >= k -> P n.
+    Proof.
+      intros P k H1 IH n H2.
+      assert (H := nat_ind (fun n => n >= k -> P n)).
+    generalize dependent n. apply H.
+      - intro H3. replace 0 with k. apply H1. inversion H3. reflexivity.
+      - intros n H0 H4. inversion H4. 
+          + rewrite H2 in H1. apply H1.
+          + apply IH. 
+             * apply H3.
+             * apply H0. apply H3.
+    Qed.
+  
+(* Exercício 88 *)
+  Lemma PIM : forall P: nat -> Prop,
+              (P 0) ->
+              (forall n, P n -> P (S n)) ->
+                forall n, P n.
+    Proof.
+      intros P H IH n.
+      apply PIG with 0.
+      - apply H.
+      - intros. apply IH. apply H1.
+      - induction n.
+          + apply le_n.
+          + apply le_S. apply IHn.
+    Qed.
+
+End ex88e87.
+
 Section aula2.
   Variables p q r t: Prop.
 
